2b3dcc4d587c4208ee0cd111935bead08ba5fe1c
lucasbeltran <lucas.beltran@outlook.es>
2017-08-27 02:52:23 -0300

    Se agrega ejemplo utilizando FontAwesome para consumir los iconos, en este caso utilizando una clase estatica como diccionario

---
+++ Xamarin.Forms/Label/CustomFonts/CustomFonts/CustomFonts/Icons.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomFonts
+{
+    public static class Icons
+    {
+        public static string Search = "\uf002";
+    }
+}
